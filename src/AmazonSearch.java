2f9f5e663bc463b1b7651b4116f5d71679bcb628
Manisha Bhagat <manishabhagat41@gmail.com>
2021-06-16 14:46:43 +0530

    Asessment 1

--- src/AmazonSearch.java
+++ src/AmazonSearch.java
@@ -17,7 +17,7 @@
 			String term = null;
 			
 			System.setProperty("webdriver.chrome.driver", "C:\\\\BrowserDrivers\\\\chromedriver.exe");
-			//WebDriverManager.chromedriver().setup();
+//			WebDriverManager.chromedriver().setup();
 			WebDriver drivers = new ChromeDriver();
 			
 			drivers.get("https://www.amazon.in/");
@@ -29,25 +29,25 @@
 			
 			try {
 				Class.forName("com.mysql.cj.jdbc.Driver");
-				Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce","root","root");
-				//Connection con=DriverManager.getConnection("jdbc:mysql://3.95.238.133:3306/ecommerce","root","root");
+				Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce","root","1234");
 				Statement stmn=con.createStatement();
-				ResultSet rs=stmn.executeQuery("select * from AMAZON");
+				
+				ResultSet rs=stmn.executeQuery("select * from amazon");
 				while(rs.next()){
 					//System.out.println(rs.getString(2)+ " "+rs.getString(3));
 					category=rs.getString(2);
 					term=rs.getString(3);
-				}
-				
-				
+				}	
 			}
-			catch(ClassNotFoundException e)
-			{
+			catch(ClassNotFoundException e)	{
 				System.out.println("Class Not Found");
 			}
 			catch (SQLException e) {
 				System.out.println("SQL Exception");
 			}
+			
+			
+			
 			WebElement searchDD = drivers.findElement(By.xpath("//*[@id='searchDropdownBox']"));
 			Select optionToSelect = new Select(searchDD);
 			optionToSelect.selectByVisibleText(category);
